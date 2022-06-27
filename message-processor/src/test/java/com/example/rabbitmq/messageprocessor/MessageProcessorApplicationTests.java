1f31bf4a71078df42029e677523b702a38e99b52
Galih Pratama <oq_galih@yahoo.com>
2022-06-27 13:00:10 +0700

    complete version1

---
+++ message-processor/src/test/java/com/example/rabbitmq/messageprocessor/MessageProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rabbitmq.messageprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
